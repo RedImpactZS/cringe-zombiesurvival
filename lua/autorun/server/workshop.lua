d3f25fa09c78a515cd0e51482f5846a93842c8f7
deinferno <14363193+deinferno@users.noreply.github.com>
2020-08-27 02:32:49 +0500

    Added workshop

---
+++ lua/autorun/server/workshop.lua
@@ -0,0 +1,6 @@
+resource.AddWorkshop( "106881043" )
+resource.AddWorkshop( "124609498" )
+resource.AddWorkshop( "824184191" )
+resource.AddWorkshop( "838698869" )
+resource.AddWorkshop( "1762648583" )
+resource.AddWorkshop( "2014127405" )
