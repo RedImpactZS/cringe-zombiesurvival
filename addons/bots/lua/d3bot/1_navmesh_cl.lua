f6fd3219a52c3b68a327509d9a4b6682849ca277
Ogn1k <62839671+Ogn1k@users.noreply.github.com>
2020-09-07 02:06:57 +0500

    fuck this shit

--- addons/bots/lua/d3bot/1_navmesh_cl.lua
+++
@@ -1,8 +0,0 @@
-
-return function(lib)
-	net.Receive(lib.MapNavMeshNetworkStr, function()
-		local data = net.ReadData(net.ReadUInt(32))
-		if data ~= "" then data = util.Decompress(data) end
-		lib.MapNavMesh = lib.DeserializeNavMesh(data)
-	end)
-end
