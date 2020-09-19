342c5e48d4203557009f6c872307405685cc6e33
Ogn1k <62839671+Ogn1k@users.noreply.github.com>
2020-09-19 12:02:27 +0500

    weapon_zs_rape

---
+++ gamemodes/zombiesurvival/entities/weapons/weapon_zs_rape/cl_init.lua
@@ -0,0 +1,11 @@
+
+include ("shared.lua")
+
+SWEP.DrawCrosshair		= false
+SWEP.ViewModelFOV		= 82
+SWEP.ViewModelFlip		= true
+
+SWEP.PrintName = "Rape SWEP"
+SWEP.Slot = 1
+SWEP.SlotPos = 4;
+SWEP.DrawAmmo = false;
